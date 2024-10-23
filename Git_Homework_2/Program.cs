43f92f6e0e6808881d994ab24bff5bd4f66d8eca
Yuriy Mikhaylov <yuriy_vm@inbox.ru>
2024-10-23 15:46:12 +0300

    added method Mult

--- Git_Homework_2/Program.cs
+++ Git_Homework_2/Program.cs
@@ -8,7 +8,7 @@
             int b = 2;
             int c = a + b;
 
-            int Sum (int a, int b)
+            int Sum(int a, int b)
             {
                 return a + b;
             }
@@ -18,12 +18,19 @@
                 return a / b;
             }
 
+            int Mult(int a, int b)
+            {
+                return a * b;
+            }
+
             int sum_result = Sum(a, b);
-            int div_result = Div (a, b);
-                
+            int div_result = Div(a, b);
+            int mult_result = Mult(a, b);
+
             Console.WriteLine($"Обычное сложение: {a} + {b} = {c}");
             Console.WriteLine($"Метод Sum {sum_result}");
             Console.WriteLine($"Метод Div {div_result}");
+            Console.WriteLine($"Метод Div {mult_result}");
             Console.ReadLine();
         }
     }
