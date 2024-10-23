209a6e28e81b766aaa50f6e6e487540c48295a69
Yuriy Mikhaylov <yuriy_vm@inbox.ru>
2024-10-23 15:11:26 +0300

    satart app

---
+++ Git_Homework_2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Git_Homework_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 25;
+            int b = 2;
+            int c = a + b;
+            Console.WriteLine($"{a} + {b} = {c}");
+            Console.ReadLine();
+        }
+    }
+}
