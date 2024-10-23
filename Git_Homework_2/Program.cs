0d2b2d3a105592c42f4aac418125a4ed6daa5cd8
Yuriy Mikhaylov <yuriy_vm@inbox.ru>
2024-10-23 15:27:59 +0300

    added method Div

--- Git_Homework_2/Program.cs
+++ Git_Homework_2/Program.cs
@@ -7,15 +7,23 @@
             int a = 25;
             int b = 2;
             int c = a + b;
+
             int Sum (int a, int b)
             {
                 return a + b;
             }
 
-            int result = Sum (a, b);
+            double Div(int a, int b)
+            {
+                return a + b;
+            }
+
+            int sum_result = Sum(a, b);
+            double div_result = Div (a, b);
                 
             Console.WriteLine($"Обычное сложение: {a} + {b} = {c}");
-            Console.WriteLine($"Метод Sum {result}");
+            Console.WriteLine($"Метод Sum {sum_result}");
+            Console.WriteLine($"Метод Div {div_result}");
             Console.ReadLine();
         }
     }
